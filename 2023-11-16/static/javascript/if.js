0c41f2c26de790f1ea743c480858fb1c21f97556
jeonyoonjingit config --global user.email <tgdash5063@naver.com>
2023-11-17 09:34:07 +0900

    2023-11-16

--- 2023-11-16/static/javascript/if.js
+++ 2023-11-16/static/javascript/if.js
@@ -1,144 +1,157 @@
-// var num = 20;
-
-// if(num>0){
+//if문 - 조건식의 참,거짓에의해 내용을 실행 시킨다.
+/* 
+if( 조건식 ){
+    참일 경우 실행할 내용
+}
+if뒤에 작은괄호열어서 안에 조건식을 작성한다.  조건식 뒤에는 중괄호를 열어서
+안에  참일 경우 실행할 내용을 작성한다.
+if( 조건식 ){
+    참일 경우 실행할 내용
+}
+else{
+    거짓일 경우 실행할 내용
+}
+조건식 거짓이라면 첫번째중괄호는 건너뛰고(jmp) else 뒤의 중괄호를 실행 시킨다.
+if문에서 조건식 작성할수 있는곳은 오직 if 뒤에만 가능하다. 
+else 뒤에는 조건식 넣을수 없다.
+조건에 따라  실행될 내용이 여러개 라면( 3개 이상)
+if( 조건식 ){
+    내용
+}
+else {
+    if( 조건식){
+        내용
+    }else{
+        내용
+        내용
+    }
+}
+if( 조건식 ){
+    내용
+}else if(조건식){
+    내용
+}else{
+    내용
+}
+*/
+// var num = 20; // num이라는 변수공간에 20을 저장해라.
+// // (num>0) ? console.log("양수다") : console.log("음수다");
+// if( num > 0){
 //     console.log("양수다");
 // }else{
 //     console.log("음수다");
 // }
-
-// var birth = parseInt(prompt(" 생년월일(8자리) 입력 "));
-// if(birth<=20041231){
+// var birth = parseInt(prompt( "생년월일(8자리) 입력" ));
+// //  birth = 19991204
+// if ( birth <= 20041231 ){
 //     console.log("성인이다");
 // }else{
-//     console.log("미성년자다");
+//     console.log("미성년자입니다.");
 // }
+//     // 국어, 영어 ,수학 3과목 점수 입력받기
+//     //  3과목의 평균점수가  65점 이상이면 통과 라고 출력
+//     //  65점 미만이면 낙제 라고 출력 하시오.
+//     var kor = parseInt(prompt("국어점수"));
+//     var eng = parseInt(prompt("영어점수"));
+//     var mat = parseInt(prompt("수학점수"));
+//     var avg = (kor+eng+mat)/3;
+//     if( avg >= 65 ){
+//         console.log("합격,통과");
+//     }else{
+//         console.log("낙제");
+//     }
+// 동전 앞면 뒷면 맞추기
+//  css 선택자  id- #,  class - .
 
-
-// var kor = parseInt(prompt(" 국어 점수 입력 "));
-// var eng = parseInt(prompt(" 영어 점수 입력 "));
-// var mat = parseInt(prompt(" 수학 점수 입력 "));
-// avg= (kor+eng+mat)/3;
-// if(avg>=65){
-//     console.log("통과");
-// }else{
-//     console.log("낙제");
-// }
-
-//동전 앞면 뒷면 맞추기
-
-//css 선택자 id-#, class -.
-
-//브라우저에 html 태그가 모두 로딩(화면표시)되면 자바스크립트 코드를 실행시키는 방법
+// 브라우저에 html태그가 모두 로딩(화면표시)되면 자바스크립트 코드를 실행시키는방법
+var com = Math.floor(Math.random()*3)+1;
 
 window.onload=function(){
 
-    //가위 바위 보 게임 만들기
-    // 1. 가위 2. 바위 3. 보
-    var ans = Math.floor(Math.random()*3)+1;
-    //var game= parseInt(prompt("1. 가위 2. 바위 3. 보"));
+    // 가위 바위 보 게임 만들기 
+    // 1.가위  2.바위  3.보
+
+    var com = parseInt(Math.random()*3)+1;
+    var user = parseInt(prompt("1.가위  2.바위  3.보"));
+
+    // var user = parseInt(prompt("1.가위  2.바위  3.보"));
 
     var scis = document.getElementById("scissors");
     var rock = document.getElementById("rock");
     var paper = document.getElementById("paper");
 
-
-    scis.addEventListener("click", function(){  //가위 클릭
-        if( ans ==1 ){
+    scis.addEventListener("click",function(){  // 가위클릭
+        if( com == 1 ){
             alert("비김");
-        }else if( ans==3){
-            alert("승");
-        }else{
-            alert("패");
-        }
-        ans = Math.floor(Math.random()*3)+1;
+        }else if( com==3 ){   alert("승");  }
+        else{  alert("패");   }
+        com = Math.floor(Math.random()*3)+1;
         scis.style.background="black";
         rock.style.background="white";
         paper.style.background="white";
     });
-    rock.addEventListener("click", function(){  //바위 클릭
-        if( ans ==2 ){
-            alert("비김");
-        }else if( ans==1){
-            alert("승");
-        }else{
-            alert("패");
-        }
-        ans = Math.floor(Math.random()*3)+1;
-        scis.style.background="white";
-        rock.style.background="black";
-        paper.style.background="white";
+    rock.addEventListener("click",function(){ // 바위 클릭
+        if( com == 2 ){ alert("비김");  }
+        else if( com == 1 ) { alert("승"); }
+        else{ alert("패"); }
+        com = Math.floor(Math.random()*3)+1;
+    }); 
+    paper.addEventListener("click",function(){  //보 클릭
+        if( com == 3 ){ alert("비김");  }
+        else if( com == 2 ) { alert("승"); }
+        else{ alert("패"); }
+        com = Math.floor(Math.random()*3)+1;
     });
-    paper.addEventListener("click", function(){  //보 클릭
-        if( ans ==3 ){
-            alert("비김");
-        }else if( ans==2){
-            alert("승");
-        }else{
-            alert("패");
-        }
-        ans = Math.floor(Math.random()*3)+1;
-        scis.style.background="white";
-        rock.style.background="white";
-        paper.style.background="black";
-    });
-    
-    // if(ans==game){
-    //     alert(" 비겼음 ");
-    // }else if((ans==1 && game ==2) || (ans==2 && game ==3)|| (ans==3 && game ==1)){
-    //     alert(" 이겼음 ");
-    // }else{
-    //     alert(" 졌음 ");
-    // }
 
-    // //세 개의 정수를 입력 받아 가장 작은 수 출력, 모두 같다면 같다 출력
-    // var num1= parseInt(prompt("첫 번째 정수"));
-    // var num2= parseInt(prompt("두 번째 정수"));
-    // var num3= parseInt(prompt("세 번째 정수"));
-
-    // if( num1==num2 && num1==num3 ){
-    //     alert("모두 같은 숫자");
-    // }else if( num1<num2 && num1<num3 ){
-    //     alert("작은수: " + num1);
-    // }else if( num2<num1 && num2<num3 ){
-    //     alert("작은수: " + num2);
+    if( com == user){
+        alert("비김");
+    }else if( (com==1&&user==2) || (com==2 && user==3) || (com==3 && user==1)  ){
+        alert("승");
+    }else{
+        alert("패");
+    }
+    //세개의 정수를 입력받아 가장 작은수 출력 , 모두 같다면 같다 출력
+    // var num1= parseInt(prompt("첫번째 정수"));
+    // var num2 = parseInt(prompt("두번째 정수 "));
+    // var num3 = parseInt(prompt("세번째 정수 "));
+    // if( num1 == num2 && num1 == num3){
+    //     alert("모두 같은숫자");
+    // }else if( (num1<num2 || num1==num2 ) && (num1<num3 || num1==num3) ){
+    //     alert("작은수 : "+num1);
+    // }else if( (num2<num1 || num2==num1) && (num2<num3|| num2==num3) ){
+    //     alert("작은수 : " + num2);
     // }else if( num3<num1 && num3<num2 ){
-    //     alert("작은수: " + num3);
+    //     alert("작은수 : "+ num3);
     // }
-
     // if(num1 < num2){
-    //     if(num1<num3){
-    //         alert("작은수: " +num1);
+    //     if( num1 < num3 ){
+    //          alert("작은수 : " +  num1);
     //     }else{
-    //         alert("작은수: " +num3);
+    //         alert("작은수 : "+num3);
     //     }
-    // }else if(num2 < num3){
-    //     alert("작은수: " +num2);
+    // }else if( num2 < num3){
+    //     alert("작은수 : " +num2);
     // }else{
-    //     alert("작은수: " +num3);
+    //     alert("작은수 : "+num3);
     // }
-    
-
-
-
     // var coin = Math.floor(Math.random()*2)+1;
-
-    // //javascript html 태그 선택방법
+    // //javascript html태그 선택방법
     // var front = document.getElementById("front");
     // var back = document.getElementById("back");
-
     // front.addEventListener("click", function(){
-    //     if(coin==1){
-    //     alert(" 정답 "); //alert는 알림창을 나타낸다.
+    //     if( coin == 1 ){
+    //         alert(" 정답 !");
     //     }else{
-    //         alert("땡");
+    //         alert("땡!!");
     //     }
+    //     //alert(" 앞면 선택 ");  // alert는 알림창을 나타내는 함수이다.
     // });
-
-    // back.addEventListener("click", function(){
-    //     if(coin==2){
-    //         alert(" 정답 "); //alert는 알림창을 나타낸다.
-    //         }else{
-    //             alert("땡");
-    //         }
+    // back.addEventListener("click",function(){
+    //     if( coin == 2 ){
+    //         alert("정답!");
+    //     }else{
+    //         alert("땡!");
+    //     }
+    //     //alert("뒷면 선택");
     // });
 };
--- 2023-11-16/static/javascript/if_study.js
+++ 2023-11-16/static/javascript/if_study.js
@@ -1,56 +1,184 @@
-// 브라우저에 html 태그가 모두 로드(화면표시) 되면 실행되는 스크립트 코드
-//
-window.onload=function(){
+//브라우저에 html태그가 모두 로드(화면표시) 되면 실행되는 스크립트 코드
+window.onload=function(){  
 
-    //주사위 게임 만들기
-    //컴퓨터와 주사위 게임을 한다.
-    //각자 주사위 두개씩 던진다. 주사위 합이 큰쪽이 이긴다.
-    //단, 두 주사위값이 같은 숫자가 나온쪽이 이긴다.
-    //둘 다 같은숫자가 나온다면 합이 큰 쪽이 이긴다.
+    // 조건문 - if문,  switch문
 
-    var com1 = Math.floor(Math.random()*6)+1;
-    var user1 = Math.floor(Math.random()*6)+1;
-    var com2 = Math.floor(Math.random()*6)+1;
-    var user2 = Math.floor(Math.random()*6)+1;
-    
-    var user = user1+user2;
-    var com =  com1+com2;
+    // if( a==10 ){
+    //     alert("같다");
+    //      a=a+10;
+    //      a=a++;
+    //      alert(a);
+    // }else{
+    //     alert("같지않다");
+    // }
 
-    if( user > com ){
-        alert("승");
-    }else if( user<com){
-        alert("패");
-    }else{
-        alert("비김");
+
+        // if(a>10){
+        //     break;//오류
+        // }
+
+
+    // var a=234;
+    // switch(a){
+    //     case 1:
+    //         alert("1");
+    //         break;
+    //     case 1000:
+    //         alert("1000");
+    //         break;
+    //     case 234:
+    //         alert("234");
+    //         break;
+    //     case 10:
+    //         alert("10 선택");
+    //         break;
+    // }
+
+    var airjordan=2;
+    var airmax=97;
+    var alp = "나이키 에어포스 90 재고";
+    switch( alp.split(" ")[5] ){
+        case "에어조던":
+            document.write("현재 재고 수량은 "+ airjordan+"개");
+            break;
+        case "에어맥스":
+            document.write("현재 재고수량은 "+ airmax+"개");
+            break;
+        case "a":
+            document.write("apple");
+            break;
+        default:
+            document.write("죄송합니다.");
     }
 
-    var user = document.getElementById("주사위");
+    //과제
+    //  주차비 정산
+    //  apple주차장의 주차요금 표
+    //   기본 주차비 - 1000원,  기본 시간- 30분
+    //   ( 30분 이내 주차시 1000원 ,  5분 주차해도 1000원, 39분까지 1000원)
+    //   10분당 100원씩 추가  ( 40분 주차시 1100원, 54분 주차시 1200원 )
+
+    //   2시간 이상 주차시 기본요금 변경 
+    //             기본요금 - 1500원 
+    //   4시간 이상 주차시 기본요금 변경
+    //             기본요금 - 2500원
+    //   8시간 이상 주차시 요금은 무조건 10000원 
+    //            (10시간,12시간, 9시간43분   전부다 10000원 )
+
+    //             (2시간 1분주차시 - 1500원, 2시간 34분주차시 - 1800원)
+    //  주차시간을 분단위로 입력하여  주차요금이 얼마인지 출력하시오
 
 
-    // addEventListener("click", function(){  //가위 클릭
-    //     if( user == dice ){
-    //         alert("비김");
-    //     }else if( user<dice){
+
+    var time = parseInt(prompt("주차시간"));
+    var cost =1000;
+
+    if(time >= 480){
+        cost=10000; time=0;
+    }else if( time>= 240){
+        cost=2500;  time = time-240;
+    }else if( time >= 120){
+        cost=1500;  time= time-120;
+    }else{
+        time = time<30 ? 0 : time-30;
+    }
+
+    var price = Math.floor( time/10)*100 + cost;
+    document.write(`주차요금 : ${price}원`);
+
+
+
+
+/*
+    if( score >=90)
+    else if(score>=80){}
+    else if( score >= 70 ){}
+    else {}
+*/
+
+    var score = 75;
+    switch( parseInt(score/10) ){
+        case 10: case 9:
+            document.write("A학점"); break;
+        case 8:
+            document.write("B학점"); break;
+        case 7:
+            document.write("C학점"); break;
+        default:
+            document.write("F학점");
+    }
+
+
+
+
+    // 주사위 게임 만들기
+    // 컴퓨터와 주사위 게임을 한다.
+    // 각자 주사위 두개씩 던진다.   주사위 합이 큰쪽이 이긴다.
+    // 단, 두주사위값이 같은 숫자가 나온쪽이 이긴다.
+    //  둘다 같은숫자가 나온다면 합이 큰쪽이 이긴다.
+
+    // var com1 = Math.floor(Math.random()*6)+1;
+    // var com2 = Math.floor(Math.random()*6)+1;
+    // var user1 = Math.floor(Math.random()*6)+1;
+    // var user2 = Math.floor(Math.random()*6)+1;
+
+    // var user = user1+user2;
+    // var com = com1+com2;
+
+
+
+    // if( user > com ){
+    //     if( (com1 == com2) && (user1 != user2)  ){
+    //         alert("패");
+    //     }else{
+    //         alert("승");
+    //     }
+    // }else if( user < com ){
+    //     if( (user1==user2) && (com1 != com2)){
     //         alert("승");
     //     }else{
     //         alert("패");
     //     }
-    // });
+    // }else{
+    //     if( (user1==user2) && (com1 != com2) ){
+    //         alert("승");
+    //     }else if( (user1!=user2) && ( com1 == com2) ){
+    //         alert("패");
+    //     }else{
+    //         alert("무");
+    //     }
+    // }
+
 }
+var parkingTime=parseInt(prompt(" 주차시간을 분단위로 입력하세요 "));
+var money = 1000 ;
 
-// 과제
-// 주차비 정산
-// apple 주차장의 주차요금 표
-// 기본 주차비 -1000원, 기본 시간 -30분
-// (30분 이내 주차시 1000원, 5분 주차해도 1000원, 39분까지 1000원)
-// 10분당 100원씩 추가 (40분 주차시 1100원, 54분 주차시 1200원)
+if(parkingTime<120){
+    if(parkingTime<40){
+        alert(" 주차요금은 1000원입니다.");
+    }
+    else{
+        parkingTime =(parkingTime-30)/10;
+        money = (parseInt(money + parkingTime * 100))/100;
+        money = (parseInt(money))*100;
 
-// 2시간 이상 주차시 기본요긍 변경
-// 기본요금 - 1500원
-// 4시간 이상 주차시 기본요금 변경
-// 기본요금 - 2500원
-// 8시간 이상 주차시 요금은 무조건 10000원
-// (10시간, 12시간 9시간 43분 모두다 10000원)
+        alert("주차요금은 " + money + "원 입니다.");
+    }
+} else if(parkingTime>=120&& parkingTime <240){
+    money = 1500;
+    parkingTime =(parkingTime-120)/10;
+    money = (parseInt(money + parkingTime * 100))/100;
+    money = (parseInt(money))*100;
 
-// (2시간 1분 주차시 1500원, 2시간 34분 주차시 1800원)
-// 주차시간을 분단위로 입력하여 주차요금이 얼마인지 출력하시오.
+    alert("주차요금은 " + money + "원 입니다.");
+}
+else if(parkingTime>=240&& parkingTime <480){
+    money = 2500;
+    parkingTime =(parkingTime-240)/10;
+    money = (parseInt(money + parkingTime * 100))/100;
+    money = (parseInt(money))*100;
+
+    alert("주차요금은 " + money + "원 입니다.");
+}else{
+    alert("주차요금은 10000원 입니다.");
+}
